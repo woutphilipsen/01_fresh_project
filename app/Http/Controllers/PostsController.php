489228e15190e275e19e9bcdbc1a1f2f314739a2
philipsenwout <philipsenwout@outlook.com>
2020-03-10 11:58:04 +0100

    06. Setting up a database connection & retrieve data to display OOP

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,22 +6,12 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
-        // Setup wildcards for the slug in associative array
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post',
-            'my-second-post' => 'Now I am getting the hang of this blogging thing'
-        ];
+        $post = \DB::table('posts')->where('slug', $slug)->first();
 
-        // Generate a 404 page when wildcard slugs are not found in $posts
-        if (! array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found.');
-        }
-
-        // If we do find the slug we provide the value associated with that slug
         return view('post', [
-            'post' => $posts[$post]
+            'post' => $post
         ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,6 +7,6 @@
 </head>
 <body>
     <h1>My Blog Post</h1>
-    <h3>{{ $post }}</h3>
+    <h3>{{ $post->body }}</h3>
 </body>
 </html>
