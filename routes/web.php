2a6ebbf1417e44348f344411098c5183276f6089
philipsenwout <philipsenwout@outlook.com>
2020-03-10 11:23:53 +0100

    04. Routing Wildcards

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Generate a test view
 Route::get('test', function() {
     return view('test');
 });
 
+// Pass & Request data to your views
 Route::get('/', function () {
     return view('test', [
         'name' => request('name')
     ]);
 });
 
+// Render the welcome page
 Route::get('/welcome', function () {
     return view('welcome');
+});
+
+// setup a route to accept wildcards
+// wildcard is available in callback function
+Route::get('/posts/{post}', function ($post) {
+    // Setup wildcards for the slug in associative array
+    $posts = [
+        'my-first-post' => 'Hello, this is my first blog post',
+        'my-second-post' => 'Now I am getting the hang of this blogging thing'
+    ];
+
+    // Generate a 404 page when wildcard slugs are not found in $posts
+    if (! array_key_exists($post, $posts)) {
+        abort(404, 'Sorry, that post was not found.');
+    }
+
+    // If we do find the slug we provide the value associated with that slug
+    return view('post', [
+        'post' => $posts[$post]
+    ]);
 });
