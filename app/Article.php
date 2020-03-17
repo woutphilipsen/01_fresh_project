937857b7af0250c90b4ef74dc3e2571b938ea1cf
philipsenwout <philipsenwout@outlook.com>
2020-03-17 18:43:46 +0100

    27. Many to Many Relationships with linking tables -> project <- users -> articles <-> tags

--- app/Article.php
+++ app/Article.php
@@ -19,4 +19,10 @@ class Article extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
 }
